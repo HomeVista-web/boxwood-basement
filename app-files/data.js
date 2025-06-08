848dc12725ede90ce1fe6191969293ba185540ca
HomeVista-web <homevistastudios@gmail.com>
2025-06-07 21:58:13 -0400

    Upload

---
+++ app-files/data.js
@@ -0,0 +1,760 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-entrance",
+      "name": "Entrance",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.16753956556754268,
+          "pitch": 0.3972281615268223,
+          "rotation": 0,
+          "target": "10-stairs"
+        },
+        {
+          "yaw": 3.060637992642583,
+          "pitch": 0.22840298760376854,
+          "rotation": 0,
+          "target": "3-utility-room"
+        },
+        {
+          "yaw": 1.2987206749733833,
+          "pitch": 0.4838280709641829,
+          "rotation": 0,
+          "target": "2-spare-bedroom"
+        },
+        {
+          "yaw": 2.5219395960433646,
+          "pitch": 0.2954548586475614,
+          "rotation": 5.497787143782138,
+          "target": "1-living-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-living-room",
+      "name": "Living room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.15598877218500462,
+          "pitch": 0.5833931083956223,
+          "rotation": 0,
+          "target": "4-living-room-seating"
+        },
+        {
+          "yaw": -2.526440130752725,
+          "pitch": 0.24270296856905915,
+          "rotation": 0,
+          "target": "0-entrance"
+        },
+        {
+          "yaw": -2.018013327926015,
+          "pitch": 0.21256260592062937,
+          "rotation": 0.7853981633974483,
+          "target": "2-spare-bedroom"
+        },
+        {
+          "yaw": 1.507643977272675,
+          "pitch": 0.40138400178430267,
+          "rotation": 6.283185307179586,
+          "target": "5-hallway"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.007412689064763001,
+          "pitch": -0.13761719621843405,
+          "title": "Custom decorative Wall",
+          "text": "1\"x2\" Oak on PET mirror sheet rolls"
+        },
+        {
+          "yaw": -0.37607074476515834,
+          "pitch": 0.43965603310516066,
+          "title": "STORKLINTA",
+          "text": "<br>"
+        },
+        {
+          "yaw": 0.5607495034062442,
+          "pitch": 1.1292412339255549,
+          "title": "STOENSE",
+          "text": "5'7\"x7'10\""
+        },
+        {
+          "yaw": -0.5663450654690259,
+          "pitch": 0.6197835910315135,
+          "title": "SONGMICS Piano Stool",
+          "text": "<br>"
+        },
+        {
+          "yaw": 0.3239778625479186,
+          "pitch": 0.39970987367825117,
+          "title": "FRIHETEN",
+          "text": "<br>"
+        },
+        {
+          "yaw": 0.4047280624632936,
+          "pitch": 0.7236564659378608,
+          "title": "LACK Coffee Table",
+          "text": "35\"x21\""
+        },
+        {
+          "yaw": 2.20846690514969,
+          "pitch": 0.5786374055931027,
+          "title": "SALJAN",
+          "text": "Oak laminate countertop"
+        },
+        {
+          "yaw": 3.0135069463615993,
+          "pitch": -0.026556888166997084,
+          "title": "LACK",
+          "text": "White oak shelves"
+        },
+        {
+          "yaw": 2.2570483351900297,
+          "pitch": -0.39399719289575685,
+          "title": "MSI Durban Anthracite Polished",
+          "text": "48\"x24\""
+        },
+        {
+          "yaw": 1.7010809499690973,
+          "pitch": 0.2890457363842529,
+          "title": "MALM",
+          "text": "Dressing table white oak veneer"
+        },
+        {
+          "yaw": 0.5835651309297702,
+          "pitch": -0.7700293724623499,
+          "title": "SOLKLINT",
+          "text": "<br>"
+        },
+        {
+          "yaw": 2.2594559461453745,
+          "pitch": 0.7728135154744429,
+          "title": "VOXTORP",
+          "text": "Custom base cabinet"
+        }
+      ]
+    },
+    {
+      "id": "2-spare-bedroom",
+      "name": "Spare Bedroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -1.2133394703768268,
+        "pitch": 0.010319924211076525,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5842697228230191,
+          "pitch": 0.15987269076921962,
+          "rotation": 0,
+          "target": "0-entrance"
+        },
+        {
+          "yaw": 1.2401363196389017,
+          "pitch": 0.17600917776820246,
+          "rotation": 5.497787143782138,
+          "target": "1-living-room"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.9882497404137673,
+          "pitch": -1.3240955869350977,
+          "title": "SOLKLINT",
+          "text": "<br>"
+        },
+        {
+          "yaw": 2.342461238417018,
+          "pitch": 0.24414555322533715,
+          "title": "Closet",
+          "text": "60\"x80\""
+        }
+      ]
+    },
+    {
+      "id": "3-utility-room",
+      "name": "Utility Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.8067983226276123,
+          "pitch": 0.27472454834301985,
+          "rotation": 0,
+          "target": "0-entrance"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.37829405076998057,
+          "pitch": -0.09947194505119938,
+          "title": "LG Washtower",
+          "text": "WKHC152HWA Heat Pump 3.1 cu.ft. Washer Ductless Dryer Combo"
+        }
+      ]
+    },
+    {
+      "id": "4-living-room-seating",
+      "name": "Living Room seating",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7650295546119299,
+          "pitch": 0.5896429418866891,
+          "rotation": 0,
+          "target": "1-living-room"
+        },
+        {
+          "yaw": 1.0856290627669303,
+          "pitch": 0.12953553604412704,
+          "rotation": 0.7853981633974483,
+          "target": "2-spare-bedroom"
+        },
+        {
+          "yaw": 0.8055317140132736,
+          "pitch": 0.1361059638335469,
+          "rotation": 0,
+          "target": "0-entrance"
+        },
+        {
+          "yaw": -0.6916917371302276,
+          "pitch": 0.3129925910918381,
+          "rotation": 4.71238898038469,
+          "target": "5-hallway"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-hallway",
+      "name": "Hallway",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.030935356807615833,
+          "pitch": 0.11733228453375588,
+          "rotation": 0,
+          "target": "2-spare-bedroom"
+        },
+        {
+          "yaw": 0.8493915732218902,
+          "pitch": 0.365078550774804,
+          "rotation": 0,
+          "target": "4-living-room-seating"
+        },
+        {
+          "yaw": 0.028728635846668737,
+          "pitch": 0.43726455175458767,
+          "rotation": 0,
+          "target": "1-living-room"
+        },
+        {
+          "yaw": -3.0561186835717464,
+          "pitch": 0.13139905093216342,
+          "rotation": 0,
+          "target": "7-master-bedroom-"
+        },
+        {
+          "yaw": -2.7404083917069553,
+          "pitch": 0.20932988063345093,
+          "rotation": 1.5707963267948966,
+          "target": "8-kitchen"
+        },
+        {
+          "yaw": 2.981648316091209,
+          "pitch": 0.21084200394627572,
+          "rotation": 4.71238898038469,
+          "target": "6-bathroom"
+        },
+        {
+          "yaw": -0.11806914275995695,
+          "pitch": 0.1776501570643454,
+          "rotation": 4.71238898038469,
+          "target": "0-entrance"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.369858946950167,
+          "pitch": 0.1638201041928049,
+          "title": "Closet",
+          "text": "36\"x80\""
+        }
+      ]
+    },
+    {
+      "id": "6-bathroom",
+      "name": "Bathroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -0.6512300571509044,
+        "pitch": -0.00017241336519546735,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.4845671462521377,
+          "pitch": 0.3621518414582354,
+          "rotation": 0.7853981633974483,
+          "target": "5-hallway"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.3545240843118691,
+          "pitch": -0.009439526432318601,
+          "title": "MATIS Collection",
+          "text": "60\" Medicine cabinet Bath Depot"
+        },
+        {
+          "yaw": -0.6450679424004484,
+          "pitch": 0.3177389700408568,
+          "title": "Wall Slats",
+          "text": "1\"x1\" Oak over black matte painted wall"
+        },
+        {
+          "yaw": 0.14321938750125973,
+          "pitch": 0.744616975130274,
+          "title": "SITKA Collection",
+          "text": "32\" Glossy white vanity with porcelain top Bath Depot"
+        },
+        {
+          "yaw": -2.3262223936317294,
+          "pitch": 0.22955906009165972,
+          "title": "MSI Urban Ridge Natural",
+          "text": "16\"x48\" Matte ceramic fluted tiles"
+        },
+        {
+          "yaw": -1.1198563509858133,
+          "pitch": 1.3533299472188904,
+          "title": "ANATOLIA Calacatta Grigio",
+          "text": "12\"x24\" Rectified porcelain tiles"
+        },
+        {
+          "yaw": -2.6665626466299024,
+          "pitch": 0.448552461974149,
+          "title": "Linen Closet",
+          "text": "18\"x72\" Door"
+        }
+      ]
+    },
+    {
+      "id": "7-master-bedroom-",
+      "name": "Master Bedroom ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.7624937551244777,
+        "pitch": 0.02014553815152098,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6037017672641039,
+          "pitch": 0.27591094171509134,
+          "rotation": 0,
+          "target": "5-hallway"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.7608846646454577,
+          "pitch": 0.6655030635395178,
+          "title": "MALM Desk",
+          "text": "<br>"
+        },
+        {
+          "yaw": -2.310679611096065,
+          "pitch": 0.3710776445327397,
+          "title": "EKBACKEN",
+          "text": "White marble effect laminate countertops"
+        },
+        {
+          "yaw": -2.844426585903266,
+          "pitch": 0.07093291343307229,
+          "title": "LACK",
+          "text": "Stained white oak wall shelves"
+        },
+        {
+          "yaw": -2.3240396489671475,
+          "pitch": -0.4289646563046521,
+          "title": "ENHET",
+          "text": "24'x15\" Cabinets with oak effect doors"
+        },
+        {
+          "yaw": -3.112313784983556,
+          "pitch": -0.1706671649183349,
+          "title": "Wall Slats",
+          "text": "1\"x1\" Oak on matte black painted wall"
+        },
+        {
+          "yaw": -2.2536663640518455,
+          "pitch": 0.7134111343264831,
+          "title": "ENHET",
+          "text": "24\"x30\" base cabinet with white VOXTORP drawer front (custom drawer box)"
+        },
+        {
+          "yaw": 0.918683919027206,
+          "pitch": 0.5239539785362837,
+          "title": "STORKLINTA",
+          "text": "<br>"
+        },
+        {
+          "yaw": 1.2204541696027764,
+          "pitch": -1.2230250056792507,
+          "title": "SOLKINT",
+          "text": "<br>"
+        },
+        {
+          "yaw": 2.011370807704502,
+          "pitch": 0.21974605192277608,
+          "title": "Closet",
+          "text": "60\"x80\""
+        },
+        {
+          "yaw": -1.0562911460148463,
+          "pitch": 0.045947938673046806,
+          "title": "ROOMMATES Clara Jean Folklore Forest",
+          "text": "Wallpaper"
+        }
+      ]
+    },
+    {
+      "id": "8-kitchen",
+      "name": "Kitchen",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8632796255374604,
+          "pitch": 0.25187894744696315,
+          "rotation": 5.497787143782138,
+          "target": "5-hallway"
+        },
+        {
+          "yaw": 3.075386865518613,
+          "pitch": 0.6382961549377697,
+          "rotation": 0,
+          "target": "9-dining"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.3743931222508188,
+          "pitch": -0.35785981337825135,
+          "title": "ASKERSUND",
+          "text": "<br>"
+        },
+        {
+          "yaw": -0.3566885087204028,
+          "pitch": -0.13389843048729944,
+          "title": "RINGHULT",
+          "text": "Cutom cut doors"
+        },
+        {
+          "yaw": 0.32162381334942225,
+          "pitch": 0.07805111043050417,
+          "title": "FRIGIDAIRE Fridge",
+          "text": "FFET1222UV 24\" Top freezer 11.6 cu.ft"
+        },
+        {
+          "yaw": -2.429886743822845,
+          "pitch": 0.8552840629645857,
+          "title": "FRIGIDAIRE Dishwasher",
+          "text": "FDPC4314AS"
+        },
+        {
+          "yaw": 2.0717256138625393,
+          "pitch": 0.5799052632143713,
+          "title": "FRIGIDAIRE Range",
+          "text": "FCFE242CAS 24\""
+        },
+        {
+          "yaw": 1.2703258422711219,
+          "pitch": 0.5251372667237959,
+          "title": "Solid Surface Countertop",
+          "text": "<br>"
+        },
+        {
+          "yaw": -2.942416251913725,
+          "pitch": -1.383885015437217,
+          "title": "SOLKLINT",
+          "text": "<br>"
+        }
+      ]
+    },
+    {
+      "id": "9-dining",
+      "name": "Dining",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.9828648647631297,
+          "pitch": 0.5988016728205761,
+          "rotation": 0,
+          "target": "8-kitchen"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.03600653258121511,
+          "pitch": 0.1780086265081824,
+          "title": "Pantry",
+          "text": "48\"x80\""
+        }
+      ]
+    },
+    {
+      "id": "10-stairs",
+      "name": "Stairs",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -2.618311629365534,
+        "pitch": -0.007115137657242698,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.9415009485503134,
+          "pitch": 0.34832690494318364,
+          "rotation": 0,
+          "target": "0-entrance"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.2366957743082434,
+          "pitch": 0.7293439856042454,
+          "title": "Lifeproof Moose River Oak",
+          "text": "LVP with Zamma Stair Nose Moulding Color 8046"
+        },
+        {
+          "yaw": 3.0036573422556545,
+          "pitch": 0.47937785354808504,
+          "title": "STORKLINTA",
+          "text": "Dresser reduced depth to 15\""
+        },
+        {
+          "yaw": 0.07474710294564879,
+          "pitch": 0.6877725880513115,
+          "title": "Service Closet",
+          "text": "60\"x80\" Unfinished Interior"
+        },
+        {
+          "yaw": 2.849839754948367,
+          "pitch": -0.18946701610539662,
+          "title": "NISSEDAL",
+          "text": "59\"x25 5/8\""
+        }
+      ]
+    }
+  ],
+  "name": "Basement",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
